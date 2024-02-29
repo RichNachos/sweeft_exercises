a4aaf710f917f417b1cfc5cec94465523e569a51
gkldi20 <gkldi20@freeuni.edu.ge>
2024-02-29 20:09:27 +0400

    init project

---
+++ src/main/java/com/richnachos/Main.java
@@ -0,0 +1,50 @@
+package com.richnachos;
+
+import java.util.List;
+
+public class Main {
+    public class ListNode {
+        int value;
+        ListNode next;
+        public ListNode() {}
+        public ListNode(int value) {
+            this.value = value;
+        }
+        public ListNode(int value, ListNode next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public static int evaluateExpression(String expression) {
+        // TODO
+        return 0;
+    }
+
+    public static int numberOfHappyStrings(List<String> strings) {
+        // TODO
+        return 0;
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        // TODO
+        return null;
+    }
+
+    public static int[] findIntersection(int[] nums1, int[] nums2) {
+        // TODO
+        return null;
+    }
+
+    public static int lenOfLongSubarr(int[] array, int k) {
+        // TODO
+        return 0;
+    }
+
+    public static boolean isValidSequence(int[] array, int[] sequence) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) { }
+}
